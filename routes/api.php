0dab218d250fbab7a10cc9fa5616d51eb1fe0ea1
AhmedGamalOmara <ahmoodsoo6@gmail.com>
2025-03-04 16:45:26 +0200

    add data

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,17 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\CustomerSawController;
 use App\Http\Controllers\LecturerController;
 use App\Http\Controllers\GetController;
 use App\Http\Controllers\TeachController;
 use App\Http\Controllers\TeamController;
 use App\Http\Controllers\ImageController;
+use App\Http\Controllers\JoinUsController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\SawController;
 use App\Http\Controllers\ServiceController;
@@ -81,6 +84,7 @@ Route::group(['prefix'=> 'lecturer','middleware'=>'auth:sanctum'], function () {
 Route::get('lecturer/', [LecturerController::class,'index']);
 Route::get('lecturer/edit/{id}', [LecturerController::class,'show']);
 
+
 Route::group(['prefix'=> 'team','middleware'=>'auth:sanctum'], function () {
     Route::post('/', [TeamController::class,'store']);
     Route::post('/edit/{id}', [TeamController::class,'update']);
@@ -109,40 +113,58 @@ Route::get('question/', [QuestionController::class, 'index']);
 
 
 Route::group(['prefix'=> 'saws','middleware'=>'auth:sanctum'], function () {
-    Route::post('/', [QuestionController::class, 'store']); 
-    Route::post('edit/{id}', [QuestionController::class, 'update']); 
-    Route::delete('/{id}', [QuestionController::class, 'destroy']); 
+    Route::post('/', [SawController::class, 'store']); 
+    Route::post('edit/{id}', [SawController::class, 'update']); 
+    Route::delete('/{id}', [SawController::class, 'destroy']); 
 });
-Route::get('saws/edit/{id}', [QuestionController::class,'show']);
-Route::get('saws/', [QuestionController::class, 'index']); 
+Route::get('saws/edit/{id}', [SawController::class,'show']);
+Route::get('saws/', [SawController::class, 'index']); 
+
+
+Route::group(['prefix'=> 'CustomerSaws','middleware'=>'auth:sanctum'], function () {
+    Route::post('/', [CustomerSawController::class, 'store']); 
+    Route::post('edit/{id}', [CustomerSawController::class, 'update']); 
+    Route::delete('/{id}', [CustomerSawController::class, 'destroy']); 
+});
+Route::get('CustomerSaws/edit/{id}', [CustomerSawController::class,'show']);
+Route::get('CustomerSaws/', [CustomerSawController::class, 'index']); 
 
 
 Route::group(['prefix'=> 'training','middleware'=>'auth:sanctum'], function () {
-    Route::post('/', [QuestionController::class, 'store']); 
-    Route::post('edit/{id}', [QuestionController::class, 'update']); 
-    Route::delete('/{id}', [QuestionController::class, 'destroy']); 
+    Route::post('/', [TrainingController::class, 'store']); 
+    Route::post('edit/{id}', [TrainingController::class, 'update']); 
+    Route::delete('/{id}', [TrainingController::class, 'destroy']); 
 });
-Route::get('training/edit/{id}', [QuestionController::class,'show']);
-Route::get('training/', [QuestionController::class, 'index']); 
+Route::get('training/edit/{id}', [TrainingController::class,'show']);
+Route::get('training/', [TrainingController::class, 'index']); 
 
 
 Route::group(['prefix'=> 'services','middleware'=>'auth:sanctum'], function () {
-    Route::post('/', [QuestionController::class, 'store']); 
-    Route::post('edit/{id}', [QuestionController::class, 'update']); 
-    Route::delete('/{id}', [QuestionController::class, 'destroy']); 
+    Route::post('/', [ServiceController::class, 'store']); 
+    Route::post('edit/{id}', [ServiceController::class, 'update']); 
+    Route::delete('/{id}', [ServiceController::class, 'destroy']); 
 });
-Route::get('services/edit/{id}', [QuestionController::class,'show']);
-Route::get('services/', [QuestionController::class, 'index']); 
+Route::get('services/edit/{id}', [ServiceController::class,'show']);
+Route::get('services/', [ServiceController::class, 'index']); 
 
 
 Route::group(['prefix'=> 'JoinUs','middleware'=>'auth:sanctum'], function () {
-    Route::post('/', [QuestionController::class, 'store']); 
-    Route::post('edit/{id}', [QuestionController::class, 'update']); 
-    Route::delete('/{id}', [QuestionController::class, 'destroy']); 
+    Route::post('/', [JoinUsController::class, 'store']); 
+    Route::post('edit/{id}', [JoinUsController::class, 'update']); 
+    Route::delete('/{id}', [JoinUsController::class, 'destroy']); 
 });
-Route::get('JoinUs/edit/{id}', [QuestionController::class,'show']);
-Route::get('JoinUs/', [QuestionController::class, 'index']); 
+Route::get('JoinUs/edit/{id}', [JoinUsController::class,'show']);
+Route::get('JoinUs/', [JoinUsController::class, 'index']); 
 
+
+
+Route::group(['prefix' => 'booking', 'middleware' => 'auth:sanctum'], function () {
+    Route::get('/', [BookingController::class, 'index']);
+    Route::post('/', [BookingController::class, 'store']);
+    Route::get('edit/{id}', [BookingController::class, 'show']);
+    Route::post('edit/{id}', [BookingController::class, 'update']);
+    Route::delete('/{id}', [BookingController::class, 'destroy']);
+});
 
 
 Route::apiResource('ContactUs', ContactController::class);
